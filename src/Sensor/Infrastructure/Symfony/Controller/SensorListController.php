d6611ec19356c1d24b34eeabf2a3545c24cd3752
Víctor <victorlopezalonso@gmail.com>
2025-02-23 21:05:21 +0100

    docs: Add swagger examples of the responses

--- src/Sensor/Infrastructure/Symfony/Controller/SensorListController.php
+++ src/Sensor/Infrastructure/Symfony/Controller/SensorListController.php
@@ -5,6 +5,10 @@ namespace App\Sensor\Infrastructure\Symfony\Controller;
 use App\Sensor\Application\Query\ListSensors\ListSensorsQuery;
 use App\Shared\Infrastructure\Symfony\Controller\AbstractApiController;
 use OpenApi\Attributes\Get;
+use OpenApi\Attributes\Items;
+use OpenApi\Attributes\JsonContent;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\Response;
 use OpenApi\Attributes\Tag;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Messenger\Exception\ExceptionInterface;
@@ -19,6 +23,24 @@ class SensorListController extends AbstractApiController
     #[Route('/v1/sensors', name: 'sensor_list', methods: ['GET'])]
     #[Get(
         summary: 'Returns a list of sensors.',
+        responses: [
+            new Response(
+                response: 200,
+                description: 'List of sensors retrieved successfully',
+                content: [
+                    new JsonContent(
+                        type: 'array',
+                        items: new Items(
+                            properties: [
+                                new Property(property: 'id', type: 'integer', example: 1),
+                                new Property(property: 'name', type: 'string', example: 'Sensor 1'),
+                            ],
+                            type: 'object'
+                        )
+                    ),
+                ]
+            ),
+        ]
     )]
     public function index(): JsonResponse
     {
